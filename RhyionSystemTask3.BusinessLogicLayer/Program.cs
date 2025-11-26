10a0cdb0a357ce0f4792788530041afa2298410e
Malakamr740 <116198822+Malakamr740@users.noreply.github.com>
2025-11-26 18:31:45 +0200

    Add project files.

---
+++ RhyionSystemTask3.BusinessLogicLayer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RhyionSystemTask3.BusinessLogicLayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ RhyionSystemTask3.PresentationLayer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RhyionSystemTask3.PresentationLayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
